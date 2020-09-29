32e9e70412f8a66a50ccf762c7293366405ccf36
early-riser18 <justin.warambourg@gmail.com>
2020-09-29 10:58:56 +0200

    added pilot button

--- header-page.js
+++ header-page.js
@@ -3,18 +3,27 @@ let myHeader = document.createElement('div');
 myHeader.className = 'header';
 myHeader.id = 'myHeader';
 myHeader.innerHTML = 
-`    <span id="header-logo">
+`    <a href="/" id="header-logo">
+
+
        <img src="https://i.ibb.co/n36Xx6v/Logo-HD-transparent.png" alt="" height="100%">
        <p>Starter Kit</p>
-    </span>
+   
+   </a>
     <nav>
        <div class="topnav">
           <a href="/">Home</a>
+          <a class="navDropdownBtn">What We Have To Offer</a>
+
+          <span class="navDropdown" >
            <a href="/kits.html">The Kits</a>
           <a href="/delivery.html">Delivery</a>
           <a href="/upcycling.html">Upcycling</a>
-          <a href="/studentbuddy.html">Student Buddy</a>
+          <a href="/studentbuddy.html">Student Buddy</a> 
+          </span>
+
           <a href="/about-us.html">About</a>
+          <a id='ctaPilot' href="/pilot.html">Contact Us</a>
        </div>
     </nav>
    <nav id="nav-mobile"><a class="mobile-menu-btn"  onclick="openMenu()"><p>MENU</p></a>
@@ -26,6 +35,8 @@ myHeader.innerHTML =
         <a href="/upcycling.html">Upcycling</a>
         <a href="/studentbuddy.html">Student Buddy</a>
         <a href="/about-us.html">About</a>  
+        <a id='ctaPilot' href="/pilot.html">Contact Us</a>
+
         </div>
         `;
 
