3994398558e704a3c01efd4b1c2c24ce3f1de5c8
early-riser18 <justin.warambourg@gmail.com>
2020-10-07 15:55:40 +0200

    form

---
+++ contact-form-handler.php
@@ -0,0 +1,21 @@
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+
+  $email_from = 'contact@mystarterkit.nl';
+
+  $email_subject = "New Form submission";
+
+  $email_body = "You have received a new message from $name.\n".
+                          "Here is the message:\n $message".
+
+                          
+  $to = "justin@mystarterkit.nl";
+
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+?>
